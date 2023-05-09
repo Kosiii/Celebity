62c523bbc25e793e717f9a59b6802787ae25a45a
Kosiii <89554417+Kosiii@users.noreply.github.com>
2023-05-09 11:54:25 -0400

    Celebrity.java

--- Celebity.java
+++ Celebity.java
@@ -21,6 +21,16 @@ public class Celebrity   {
 
 
             public Celebrity( String gender, int age, String height, int weight, String Profession,  String Initials, String haircolor, String skincolor){
+                this.gender = gender
+                this.age = age
+                this.height = height
+                this.weight = weight
+                this.Profession = Profession    
+                this.Initals = Initals    
+                this.haircolor = haircolor    
+                this.skincolor = skincolor    
+                    
+                    
                 String [] array = {"Zion","Lebron" ,"Ja_Morant" ,"Micheal_B.Jordan" ,"Lil Uzi Vert" ,"Chris Pratt" ,"Dr.Phil", "SpongeBob","Taylor Swift","Frank_Sinatra" }; 
            
             Celebrity Lebron = new Celebrity("male", 38,"6foot8inches", 250, "Basketballplayer", "LBJ", "black", "Black");
@@ -34,11 +44,35 @@ public class Celebrity   {
         }
          
 
-
-        
-        
-
-           
-
-        
+}
+    
+    public String getGender() {
+        return gender;
     }
+    
+    public String getAge() {
+        return age;
+    }
+    
+    public String getHeight() {
+        return height;
+    }
+    
+    public String getWeight() {
+        return weight;
+    }
+      public String getProfession() {
+        return profession;
+    } 
+    public String getInitals() {
+        return Initals;
+    } 
+    public String getHaircolor() {
+        return haircolor;
+    }        
+    public String getSkincolor() {
+        return skincolor;
+    } 
+
+    }
+
