d2e883e7287f6aa425782c4ec4c8f76783ad8df6
Kosiii <89554417+Kosiii@users.noreply.github.com>
2023-05-09 12:26:45 -0400

    Celebrity.java

--- Celebity.java
+++ Celebity.java
@@ -31,16 +31,6 @@ public class Celebrity   {
                 this.skincolor = skincolor    
                     
                     
-                String [] array = {"Zion","Lebron" ,"Ja_Morant" ,"Micheal_B.Jordan" ,"Lil Uzi Vert" ,"Chris Pratt" ,"Dr.Phil", "SpongeBob","Taylor Swift","Frank_Sinatra" }; 
-           
-            Celebrity Lebron = new Celebrity("male", 38,"6foot8inches", 250, "Basketballplayer", "LBJ", "black", "Black");
-            Celebrity Zion = new Celebrity("male", 22, "6foot6inches", 270, "Basketballplayer", "ZW", "black", "Black");
-            Celebrity Ja_Morant = new Celebrity("male",23,"6foot2inches", 183, "Basketballplayer", "JM","black", "Black");
-            Celebrity Micheal_B_Jordan = new Celebrity("male",35, "5foot11inches", 175, "Actor", "MJ","black", "Black");
-            Celebrity Lil_Uzi_Vert = new Celebrity("male", 38, "5foot4inches", 150, "Rapper", "LUV", "Red", "Black" );
-            Celebrity Chris_Pratt = new Celebrity("male", 43, "6foot2inches", 185, "Actor", "CP", "blonde", "White");
-            Celebrity DrPhill = new Celebrity("male",72, "6foot3inches", 173, "Media_Personality", "DP", "grey","White");
-            Celebrity Taylor_Swift = new Celebrity("Female", 33, "5foot11inches", 119, "Actor", "TS", "blonde", "White");
         }
          
 
